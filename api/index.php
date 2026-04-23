7753240db5123c515e8d1b32429580e55d87be5f
Berlin Ong <Berlin.ong.karokaro@gmail.com>
2026-04-23 18:38:50 +0700

    Fix Storage path for vercel

--- api/index.php
+++ api/index.php
@@ -5,9 +5,31 @@ error_reporting(E_ALL);
 
 define('LARAVEL_START', microtime(true));
 
+// Use /tmp for writable directories on Vercel
+$_ENV['APP_STORAGE'] = '/tmp/storage';
+
+if (!is_dir('/tmp/storage/logs')) {
+    mkdir('/tmp/storage/logs', 0755, true);
+}
+if (!is_dir('/tmp/storage/framework/cache')) {
+    mkdir('/tmp/storage/framework/cache', 0755, true);
+}
+if (!is_dir('/tmp/storage/framework/sessions')) {
+    mkdir('/tmp/storage/framework/sessions', 0755, true);
+}
+if (!is_dir('/tmp/storage/framework/views')) {
+    mkdir('/tmp/storage/framework/views', 0755, true);
+}
+if (!is_dir('/tmp/bootstrap/cache')) {
+    mkdir('/tmp/bootstrap/cache', 0755, true);
+}
+
 require __DIR__ . '/../vendor/autoload.php';
 
 $app = require __DIR__ . '/../bootstrap/app.php';
+
+$app->useStoragePath('/tmp/storage');
+$app->bootstrapPath('/tmp/bootstrap');
 
 try {
     $kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
