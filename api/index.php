bc4067b43b74e80504b9910db1d76fd6ec75cad1
Berlin Ong <Berlin.ong.karokaro@gmail.com>
2026-04-23 18:31:11 +0700

    Test autoload

--- api/index.php
+++ api/index.php
@@ -1,2 +1,10 @@
 <?php
-echo "Hello World!";
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+error_reporting(E_ALL);
+
+define('LARAVEL_START', microtime(true));
+
+require __DIR__ . '/../vendor/autoload.php';
+
+echo "Autoload OK!";
